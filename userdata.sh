505959f351f53216dd87e868bf1abb1a4d734d2e
Kalai-GitHub <kalaiselvimurugesan85@gmail.com>
2024-07-09 20:48:47 -0700

    clone card-website repo and restart the apache

--- userdata.sh
+++ userdata.sh
@@ -3,3 +3,7 @@
 sudo apt-get update
 sudo apt-get install apache2 -y
 mkdir -p /home/ubuntu/dir1
+sudo rm -rf /var/www/html/*
+sudo git clone https://github.com/amolshete/card-website.git /var/www/html
+sudo chown -R www-data:www-data /var/www/html
+sudo systemctl restart apache2
